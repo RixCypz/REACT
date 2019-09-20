bb186cf8032170f099ee4ec1e1bf879d5c7bc43d
unknown <RixCypz>
2019-09-21 02:09:09 +0700

    1React

---
+++ card-game/src/WordCard.js
@@ -0,0 +1,43 @@
+import _ from'lodash';
+import React, { Component } from 'react'
+import CharacterCard from './CharacterCard';
+
+
+const prepareStateFromWord = (value) => {
+    let word = value.toUpperCase()
+    // let chars = _.shuffle(Array.from(word))
+    return{
+        word
+        // chars,
+        // attempt: 1,
+        // guess: [],
+        // completed: false
+    }
+}
+
+export default class WordCard extends Component {
+    
+    activationHandler= c=>{console.log(`${c} has been activated.`) }
+
+    // activationHandler = (c) =>{
+    //     letguess = [...this.state.guess, c]
+    //     this.setState({guess})
+    //     if(guess.length== this.state.chars.length){
+    //         if(guess.join('').toString() == this.state.word){
+    //             this.setState({guess:[], completed:true})
+    //         }else{
+    //             this.setState({guess:[], attempt:this.state.attempt+ 1})
+    //         }
+    //     }
+    // }
+
+    render() {
+        return (
+            <div>
+                { Array.from(this.props.value).map((c, i) => <CharacterCard value={c} key={i} activationHandler = {this.activationHandler}/>) }
+
+
+            </div>
+        );
+    }
+}
